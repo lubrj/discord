ce9b00db1fdad3674328002cf3de448e53ad11f2
LUBRJ <145651445+lubrj@users.noreply.github.com>
2025-04-17 22:42:44 +0200

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    document.body.addEventListener('click', function() {
-      var audio = document.getElementById('audio');
-      audio.play().catch(error => {
-        console.error("Error playing the audio:", error);
-      });
-    });
-  };
